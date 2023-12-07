204a94d273ed965ccc1f94315f12dca512ce1c97
Aklilegetachew <aklilegetachew@gmail.com>
2023-12-07 08:18:27 +0300

    Primary Commit

---
+++ src/main.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './styles/tailwind.css';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import Root from './routes/Root';
+import PageNotFound from './pages/others/404';
+import Home from './pages/home';
+
+
+const NODE_ENV = import.meta.env.VITE_USER_NODE_ENV;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: NODE_ENV !== 'development' && <PageNotFound />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+
+  }
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+
+// Render the application using the root object
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
